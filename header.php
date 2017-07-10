6a1dacfde927e333733810bf11cacf0e30d250b3
ALEKS <rotaralek@gmail.com>
2017-07-10 12:47:03 +0300

    Validation fixed

--- header.php
+++ header.php
@@ -1,13 +1,11 @@
 <!doctype html><!--[if lt IE 7]>
-<html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]--><!--[if IE 7]>
-<html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]--><!--[if IE 8]>
-<html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]--><!--[if gt IE 8]><!-->
-<html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
+<html itemscope class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]--><!--[if IE 7]>
+<html itemscope class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]--><!--[if IE 8]>
+<html itemscope class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]--><!--[if gt IE 8]><!-->
+<html itemscope class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>"/>
 	<meta name="viewport" content="width=device-width initial-scale=1">
-	<meta http-equiv="Expires" content="<?php $next_expires_year = date( 'Y' ) + 1;
-	echo date( 'D, d M ' ) . $next_expires_year; ?> 23:59:59 GMT">
 	<link rel="icon" href="<?php echo tell_get_option( 'opt-favicon-image', 'url' ); ?>">
 	<title><?php wp_title(); ?></title>
 	<?php wp_head(); ?>
